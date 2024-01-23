b31cb058f39eae0d2ae01cc017448d267b3aa031
조원우 <ww8637@naver.com>
2024-01-23 17:04:21 +0900

    update

--- algo_prac_1.py
+++ algo_prac_1.py
@@ -1,14 +1,21 @@
-def sum_odd(numbers):
-    odd_num = 0
+# def sum_odd(numbers):
+#     odd_num = 0
+#     for num in numbers:
+#         if num % 2 != 0:
+#             odd_num += num
+#     return odd_num
+
+
+# T = int(input())
+# for tc in range(1, T + 1):
+#     numbers = list(map(int, input().split()))
+#     result = sum_odd(numbers)
+
+#     print(f'#{tc} {result}')
+
+def find_max_num(numbers):
+    max_num = 0
     for num in numbers:
-        if num % 2 != 0:
-            odd_num += num
-    return odd_num
-
-
-T = int(input())
-for tc in range(1, T + 1):
-    numbers = list(map(int, input().split()))
-    result = sum_odd(numbers)
-
-    print(f'#{tc} {result}')
+        if num > max_num:
+            max_num = num
+    return max_num
